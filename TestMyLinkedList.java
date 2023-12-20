b57a3951d500ce75f4818e1768d23231206d2248
luugiahuy123 <luugiahuyqn@gmail.com>
2023-12-20 14:41:36 +0700

    thuc hanh

---
+++ TestMyLinkedList.java
@@ -0,0 +1,14 @@
+package ss10.TH.TrienKhaiLopLinkedList;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=/=");
+        MyLinkList n = new MyLinkList(10);
+        n.addFirst(11);
+        n.addFirst(12);
+        n.addFirst(13);
+        n.add(4,9);
+        n.add(4,9);
+
+    }
+}
